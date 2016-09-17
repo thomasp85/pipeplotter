a436aed7131dddc17fe5bfb6774215534a69a064
thomasp85 <thomasp85@gmail.com>
2016-09-17 23:09:52 +0200

    Better error handling

--- R/scales.R
+++ R/scales.R
@@ -8,6 +8,9 @@ set_scales <- function(., ...) {
     for (scale in names(scales)) {
         scale_type <- as.character(scales[[scale]]$expr)[1]
         scale_name <- paste0("scale_", scale, '_', scale_type)
+        scale_fun <- tryCatch(get(scale_name), error = function(e) {
+            stop(scale_name, ' is not available. Have you loaded the necessary package?', call. = FALSE)
+        })
         temp_envir <- structure(list(get(scale_name)), names = scale_type)
         . <- . + eval(scales[[scale]]$expr, envir = temp_envir, enclos = scales[[scale]]$env)
     }
