c8ea5f6d3e3f1d75eece2caebbc1aa61d0bf2287
thomasp85 <thomasp85@gmail.com>
2016-09-17 22:48:04 +0200

    Add set_scales

--- R/scales.R
+++ R/scales.R
@@ -1,4 +1,15 @@
+#' @importFrom lazyeval lazy_dots
 #' @export
-map_x_ <- function(., ...) {
-
+set_scales <- function(., ...) {
+    scales <- lazy_dots(...)
+    if (is.null(names(scales))) {
+        stop("scales must be named", call. = FALSE)
+    }
+    for (scale in names(scales)) {
+        scale_type <- as.character(scales[[scale]]$expr)[1]
+        scale_name <- paste0("scale_", scale, '_', scale_type)
+        temp_envir <- structure(list(get(scale_name)), names = scale_type)
+        . <- . + eval(scales[[scale]]$expr, envir = temp_envir, enclos = scales[[scale]]$env)
+    }
+    .
 }
