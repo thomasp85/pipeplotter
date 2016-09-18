ba9ed68f08d3e187967fb724187857e3263462fd
Oliver Keyes <ironholds@gmail.com>
2016-09-17 17:36:31 -0700

    Fix dot-iteration bug

--- R/scales.R
+++ R/scales.R
@@ -2,10 +2,12 @@
 #' @export
 set_scales <- function(., ...) {
     scales <- lazy_dots(...)
-    if (is.null(names(scales))) {
+    scale_names <- names(scales)
+    
+    if (is.null(scale_names) || any(scale_names == "")) {
         stop("scales must be named", call. = FALSE)
     }
-    for (scale in names(scales)) {
+    for (scale in scale_names) {
         scale_type <- as.character(scales[[scale]]$expr)[1]
         scale_name <- paste0("scale_", scale, '_', scale_type)
         scale_fun <- tryCatch(get(scale_name), error = function(e) {
